07a4af9e5d737295c0cc1787dd5ec2d88679100c
Jonas Meeuws <jonas.meeuws@gmail.com>
2024-11-14 17:52:49 +0100

    Add talk

---
+++ talk/README.md
@@ -0,0 +1,1 @@
+This contains all files related to the lightning workshop I gave at [Zeus WPI](https://zeus.ugent.be/events/24-25/lightning_workshops/).
---
+++ talk/docker-services-v1/docker-services/scripts/create-aliases.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo '' >> ~/.bashrc
+echo 'alias d="docker"' >> ~/.bashrc
+echo 'alias dc="docker compose"' >> ~/.bashrc
+echo 'alias dcps="docker compose ps -a | less -S"' >> ~/.bashrc
